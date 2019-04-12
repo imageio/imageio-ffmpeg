b17343d0d02b3a3a30d47c28960feeb9eff91330
Almar Klein <almar.klein@gmail.com>
2019-04-12 11:02:15 +0200

    Add requirement on pip 19+ in setup.py (#16)
    Add requirement on pip 19+ in setup.py

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
     platforms="any",
     provides=["imageio_ffmpeg"],
     python_requires=">=3.4",
+    setup_requires=["pip>19"],
     install_requires=[],  # todo: maybe numpy
     packages=["imageio_ffmpeg"],
     package_dir={"imageio_ffmpeg": "imageio_ffmpeg"},
