d9628b5acb81f8edab3b64f1593de498fefc3f10
lbreede <lennart.breede@gmail.com>
2023-04-06 15:58:16 -0700

    method docstrings, removed unnecessary i in loops

--- tests/test_terminate.py
+++ tests/test_terminate.py
@@ -34,6 +34,7 @@ def test_ffmpeg_version():
 
 @no_warnings_allowed
 def test_reader_done():
+    """Test that the reader is done after reading all frames"""
     for _ in range(N):
         pids0 = get_ffmpeg_pids()
         r = imageio_ffmpeg.read_frames(test_file1)
@@ -51,6 +52,7 @@ def test_reader_done():
 
 @no_warnings_allowed
 def test_reader_close():
+    """Test that the reader is done after closing it"""
     for _ in range(N):
         pids0 = get_ffmpeg_pids()
         r = imageio_ffmpeg.read_frames(test_file1)
@@ -67,6 +69,7 @@ def test_reader_close():
 
 @no_warnings_allowed
 def test_reader_del():
+    """Test that the reader is done after deleting it"""
     for _ in range(N):
         pids0 = get_ffmpeg_pids()
         r = imageio_ffmpeg.read_frames(test_file1)
@@ -84,6 +87,7 @@ def test_reader_del():
 
 @no_warnings_allowed
 def test_write_close():
+    """Test that the writer is done after closing it"""
     for _ in range(N):
         pids0 = get_ffmpeg_pids()
         w = imageio_ffmpeg.write_frames(test_file2, (64, 64))
@@ -119,8 +123,11 @@ def test_write_del():
 
 def test_partial_read():
     # Case: https://github.com/imageio/imageio-ffmpeg/issues/69
-    template = "import sys; import imageio_ffmpeg; f=sys.argv[1]; print(f); r=imageio_ffmpeg.read_frames(f);"
-    for _ in range(4):
+    template = (
+        "import sys; import imageio_ffmpeg; f=sys.argv[1]; print(f); "
+        "r=imageio_ffmpeg.read_frames(f);"
+    )
+    for i in range(4):
         code = template + " r.__next__();" * i
         cmd = [sys.executable, "-c", code, test_file1]
         result = subprocess.run(
